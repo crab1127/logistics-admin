d5e9c3520d40c4a10b5411491ae5c53d9d1ef2bb
crab1127 <crab1127@gmail.com>
2017-12-04 17:37:44 +0800

    ADD 引入国际化

---
+++ src/lang/zh.js
@@ -0,0 +1,5 @@
+export default {
+  route: {
+    a: 'a'
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en'
 import App from './App'
+import i18n from './lang'
 import router from './router'
 import store from './store'
 import '@/icons' // icon
 import '@/permission' // 权限
 
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, {
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 Vue.config.productionTip = false
 
@@ -16,6 +18,7 @@ new Vue({
   el: '#app',
   router,
   store,
+  i18n,
   template: '<App/>',
   components: { App }
 })
